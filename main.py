2c95dbf75ad0d19c01ace613beff26181ddba481
shriadke <shrinidhi.adke@gmail.com>
2023-10-09 08:58:55 -0400

    data validation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ logger.info("This Project uses custom logger.\n")
 logger.info("#################################\n")
 
 from fetchSearch.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from fetchSearch.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -10,6 +11,16 @@ try:
     logger.info(f"######## Satge {STAGE_NAME} started. ########")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f"######## Satge {STAGE_NAME} completed. ########\n\n################################################")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f"######## Satge {STAGE_NAME} started. ########")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f"######## Satge {STAGE_NAME} completed. ########\n\n################################################")
 except Exception as e:
     logger.exception(e)
--- src/fetchSearch/config/configuration.py
+++ src/fetchSearch/config/configuration.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from fetchSearch.constants import *
 from fetchSearch.utils.common import read_yaml, create_directories
-from fetchSearch.entity import DataIngestionConfig
+from fetchSearch.entity import DataIngestionConfig, DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -29,3 +29,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,            
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES 
+        )
+
+        return data_validation_config
